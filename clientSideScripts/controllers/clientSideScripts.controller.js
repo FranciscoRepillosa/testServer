b23797809b0f982dffdd101cb3d92253906cd282
FranciscoRepillosa <franciscorg834@gmail.com>
2021-04-20 11:30:28 -0300

    changing scripts paths

--- clientSideScripts/controllers/clientSideScripts.controller.js
+++ clientSideScripts/controllers/clientSideScripts.controller.js
@@ -1,4 +1,4 @@
 
 exports.sendClientSideScript = (req, res) => {
-    res.sendFile(`C:/Users/computador/Documents/1_proyecos/Pro-Proyects/Learning-Platform/Learning-platform/clientSideScripts/scripts/${req.params.script}.js`);
+    res.sendFile(`C:/Users/computador/Desktop/cloned-Test-Server/testServer/clientSideScripts/scripts/${req.params.script}.js`);
 }
